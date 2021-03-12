df6da19235644cacde4c92ca13b993061de83c17
Yen Lung Chen <jimmychen0123@gmail.com>
2021-03-12 21:14:11 +0800

    Revert "focus box extended when touching"
    This reverts commit d0e7f36f751a7858c5b32193319e0d01b8fea911.

--- scripts/attributestore.js
+++ scripts/attributestore.js
@@ -166,7 +166,6 @@ MoInVis.Paracoords.attributeStore = function ( moin, parentDiv, axes ) {
                         console.log(event.type, event.target)
                         document.querySelector('.focusPanelBar').style.width = _boxWidth().long + 'px';
                         
-                        
                     },
                     move (event) {
                         // position.x += event.dx
@@ -181,22 +180,12 @@ MoInVis.Paracoords.attributeStore = function ( moin, parentDiv, axes ) {
                         document.querySelector('.focusPanelBar').style.width = _boxWidth().short/8 + 'px';
                         
 
-                    },
-                    
+                    }
                     
                 }
-            })
-            .on('down', function (event) {
-
-                document.querySelector('.focusPanelBar').style.width = _boxWidth().long + 'px';
-                console.log(event.type, event.target)
-            })
-            .on('up', function (event) {
-
-                document.querySelector('.focusPanelBar').style.width = _boxWidth().short/8 + 'px';
-                console.log(event.type, event.target)
             });
-   
+            
+            
 
         },
 
