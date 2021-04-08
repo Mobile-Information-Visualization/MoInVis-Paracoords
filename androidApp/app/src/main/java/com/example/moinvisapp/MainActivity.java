c2b5e01e2b6e5427853480820e2c55b57ba8c90a
Josef Roth <josef.roth@uni-weimar.de>
2021-04-08 19:38:50 +0200

    Cache clearing added

--- androidApp/app/src/main/java/com/example/moinvisapp/MainActivity.java
+++ androidApp/app/src/main/java/com/example/moinvisapp/MainActivity.java
@@ -17,8 +17,8 @@ public class MainActivity extends Activity {
 
         WebView webView = findViewById(R.id.webview);
         webView.loadUrl("http://192.168.178.20:8888");
+        webView.clearCache(true);
 
-        // To call methods in Android from using js in the html, AndroidInterface.showToast, AndroidInterface.getAndroidVersion etc
 //        webView.addJavascriptInterface(new WebAppInterface(this), "AndroidInterface");
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -32,8 +32,6 @@ public class MainActivity extends Activity {
     private class MyWebChromeClient extends WebChromeClient {
         @Override
         public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
-//            Log.d("LogTag", message);
-//            result.confirm();
             return true;
         }
     }
